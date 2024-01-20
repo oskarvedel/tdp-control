0856c6653d3cbce0a3f7d97a9265e72c6d62351e
oskarvedel <oskar.vedel@gmail.com>
2024-01-20 18:46:20 +0100

    email the admin when daily functions are finished

--- tdp-control-plugin.php
+++ tdp-control-plugin.php
@@ -51,8 +51,21 @@ function run_tdp_daily_functions()
     sleep(30);
     generate_missing_chatgpt_geolocation_short_descriptions(50);
     trigger_error('FINISHED RUNNING TDP DAILY FUNCTIONS', E_USER_WARNING);
+
+    //send an email to the admin
+    $subject = "TDP Daily Functions just ran";
+    send_email("", $subject);
 }
 add_action('run_tdp_daily_functions', 'run_tdp_daily_functions');
+
+function send_email($body, $subject)
+{
+    $to = get_option('admin_email'); // Get the admin email
+
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+
+    wp_mail($to, $subject, $body, $headers);
+}
 
 
 //add a admin plugin button to run the daily functions
